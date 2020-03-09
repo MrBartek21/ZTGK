27fe838a9b8ae8cd0ec5e7d8c54a972124258ac8
MrBartek21 <bartek.pacyna@onet.pl>
2020-03-09 23:20:15 +0100

    Screen
    Screen size

--- Bartek/run.py
+++ Bartek/run.py
@@ -1,13 +1,23 @@
-import pygame, sys
+import pygame, sys, ctypes
 
-screen = pygame.display.set_mode((1280,720))
+# Screen size
+user32 = ctypes.windll.user32
+
+pygame.init()
+screen = pygame.display.set_mode((user32.GetSystemMetrics(0),user32.GetSystemMetrics(1)))
+box = pygame.Rect(10,10,200,500)
 
 while True:
     # Handle events
     for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit(0)
+    # Input
+    box.x += 1
+    box.y += 1
+    box.w += 1
 
     # Drawing
-    pygame.draw.rect(screen, (255, 0, 0), pygame.Rect(10,50,200,500))
+   #screen.fill((0,0,0))
+    pygame.draw.rect(screen, (255, 0, 0), box)
     pygame.display.flip()
