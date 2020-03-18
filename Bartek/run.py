2e83c51f0292822a640c5f8eea77e3440fea59f2
MrBartek21 <bartek.pacyna@onet.pl>
2020-03-18 16:28:35 +0100

    Update run.py

--- Bartek/run.py
+++ Bartek/run.py
@@ -6,7 +6,8 @@ import sqlite3 as lite
 con = lite.connect('database.db')
 with con:
     cur = con.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS save( id integer PRIMARY KEY, nick text NOT NULL, coin integer, world integer, lvl integer, eq integer);")
+    cur.execute(
+        "CREATE TABLE IF NOT EXISTS save( id integer PRIMARY KEY, nick text NOT NULL, coin integer, world integer, lvl integer, eq integer);")
     # cur.execute("SHOW TABLES;")
 
     # data = cur.fetchone()[0]
@@ -29,7 +30,7 @@ class Game(object):
 
         # Config
         self.tps_max = 60.0
-        self.choice = 0
+        self.choice = 'menu'
         self.box = pygame.Rect(0, 0, self.ScreenWidth2, self.ScreenHeight)
         self.DeveloperMode = True
         self.Title = "Bill's Adventrure"
@@ -84,7 +85,7 @@ class Game(object):
 
             # Drawing
             # self.screen.fill((0,0,0))
-            #print(self.choice)
+            # print(self.choice)
             self.draw()
             pygame.display.flip()
 
@@ -103,27 +104,27 @@ class Game(object):
 
     # Main draw function
     def draw(self):
-        if self.choice < 5:
-            self.screen.blit(self.background_image, [0, 0])
-            # Draw text title
-            self.draw_text(self.Title, (127, 27, 27), int(self.x / 2), 100, 96)
-
-            # Choice Menu
-            if self.choice == 0:
-                self.draw_buttons("New game", (127, 27, 27), 36, self.x, 250, 300, 64, self.new_game)
-                self.draw_buttons("Load game", (127, 27, 27), 36, self.x, 325, 300, 64, self.load_game)
-                self.draw_buttons("Multiplayer", (127, 27, 27), 36, self.x, 400, 300, 64, self.multiplayer)
-                self.draw_buttons("Change character", (127, 27, 27), 36, self.x, 475, 300, 64, self.change_charakter)
-                self.draw_buttons("Settings", (127, 27, 27), 36, self.x, 550, 300, 64, self.settings)
-                self.draw_buttons("Exit", (127, 27, 27), 36, self.x, 625, 300, 64, self.quit_game)
-            elif self.choice == 1:
-                self.draw_buttons("Music", (127, 27, 27), 36, 650, 300, 300, 64, self.music)
-                self.draw_buttons("Back", (127, 27, 27), 36, self.x, self.y - 100, 300, 64, self.back)
-        else:
-            self.screen.blit(self.bg_l1_image, [0, 0])
+        self.draw_text("Preview version. Build ", (127, 27, 27), (self.x + 100) - self.x, 300, 36)
+        # Choice Menu
+        if self.choice == 'menu':
+            self.main_menu()
+        elif self.choice == 'new_game':
+            self.new_game()
+        elif self.choice == 'load_game':
+            self.load_game()
+        elif self.choice == 'multiplayer':
+            self.multiplayer()
+        elif self.choice == 'change_character':
+            self.change_character()
+        elif self.choice == 'settings':
+            self.settings()
+        elif self.choice == 'quit_game':
+            sys.exit(0)
+        elif self.choice == 'back':
+            self.choice = 'menu'
 
     # Draw buttons function
-    def draw_buttons(self, text, text_color, size, x, y, width, height, action=None):
+    def draw_buttons(self, text, text_color, size, x, y, width, height, choice=None):
         mouse = pygame.mouse.get_pos()
         click = pygame.mouse.get_pressed()
 
@@ -133,8 +134,8 @@ class Game(object):
         if int(x / 2 - int(width / 2)) + width > mouse[0] > int(x / 2 - int(width / 2)) and y + height > mouse[1] > y:
             self.screen.blit(self.btn_image2, [int(x / 2 - int(width / 2)), y])
             self.draw_text(text, text_color, int(x / 2), int(y + int(height / 2)), size)
-            if click[0] == 1 and action != None:
-                action()
+            if click[0] == 1 and choice != None:
+                self.choice = choice
         else:
             self.screen.blit(self.btn_image, [int(x / 2 - int(width / 2)), y])
             self.draw_text(text, text_color, int(x / 2), int(y + int(height / 2)), size)
@@ -148,19 +149,47 @@ class Game(object):
         textrect.center = (x, y)
         self.screen.blit(textobj, textrect)
 
+    def main_menu(self):
+        # Background and title
+        self.screen.blit(self.background_image, [0, 0])
+        # Draw text title
+        self.draw_text(self.Title, (127, 27, 27), int(self.x / 2), 100, 96)
+
+        self.draw_buttons("New game", (127, 27, 27), 36, self.x, 250, 300, 64, 'new_game')  # 1
+        self.draw_buttons("Load game", (127, 27, 27), 36, self.x, 325, 300, 64, 'load_game')  # 2
+        self.draw_buttons("Multiplayer", (127, 27, 27), 36, self.x, 400, 300, 64, 'multiplayer')  # 3
+        self.draw_buttons("Change character", (127, 27, 27), 36, self.x, 475, 300, 64, 'change_character')  # 4
+        self.draw_buttons("Settings", (127, 27, 27), 36, self.x, 550, 300, 64, 'settings')  # 5
+        self.draw_buttons("Exit", (127, 27, 27), 36, self.x, 625, 300, 64, 'quit_game')  # 6
+
     def new_game(self):
-        self.choice = 6
+        self.screen.blit(self.bg_l1_image, [0, 0])
+        self.draw_buttons("Exit", (127, 27, 27), 36, self.x, 625, 300, 64, 'quit_game')  # 6
+
+    # Load Game function
+    def load_game(self):
+        pass
+
+    # Multiplayer function
+    def multiplayer(self):
+        pass
+
+    # CC function
+    def change_character(self):
+        pass
 
     def settings(self):
-        self.choice = 1
+        # Background and title
+        self.screen.blit(self.background_image, [0, 0])
+        # Draw text title
+        self.draw_text(self.Title, (127, 27, 27), int(self.x / 2), 100, 96)
 
-    # Exit game function
-    def quit_game(self):
-        sys.exit(0)
+        self.draw_text("Music", (127, 27, 27), (self.x+300)-self.x, 300, 36)
+        self.draw_buttons("Back", (127, 27, 27), 36, self.x, self.y - 100, 300, 64, 'back')
 
-    # Back function
-    def back(self):
-        self.choice = 0
+    # Music function
+    def music(self):
+        pass
 
 
 if __name__ == "__main__":
